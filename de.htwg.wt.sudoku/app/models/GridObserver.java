af63d3373586292997946887c815e17dbbafed3e
markoboger <marko.boger@gmail.com>
2013-11-26 23:58:22 +0100

    added a GridObserver

---
+++ de.htwg.wt.sudoku/app/models/GridObserver.java
@@ -0,0 +1,27 @@
+package models;
+
+import play.mvc.WebSocket;
+import play.mvc.WebSocket.Out;
+import de.htwg.sudoku.controller.ISudokuController;
+import de.htwg.sudoku.util.observer.Event;
+import de.htwg.sudoku.util.observer.IObserver;
+
+public class GridObserver implements IObserver {
+	
+	private Out<String> out;
+	private ISudokuController controller;
+
+
+	public GridObserver(ISudokuController controller,WebSocket.Out<String> out) {
+		controller.addObserver(this);
+		this.controller = controller;
+		this.out = out;
+	}
+
+	@Override
+	public void update(Event arg0) {
+		out.write(controller.getGrid().toJson());	
+		System.out.println("WUI was updated");
+	}
+
+}
