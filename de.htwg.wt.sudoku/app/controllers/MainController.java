1c125f9e0baf384e299ed915ed959df7a99d00a1
Patrick Kohan <patrick.kohan@googlemail.com>
2014-01-13 21:38:53 +0100

    added Google OpenID Login

--- de.htwg.wt.sudoku/app/controllers/MainController.java
+++ de.htwg.wt.sudoku/app/controllers/MainController.java
@@ -3,6 +3,10 @@ package controllers;
 import models.GridObserver;
 import play.data.*;
 import static play.data.Form.*;
+
+import play.libs.F;
+import play.libs.Json;
+import play.libs.OpenID;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
@@ -10,6 +14,9 @@ import play.mvc.Http.Context;
 import play.mvc.WebSocket;
 import de.htwg.sudoku.Sudoku;
 import de.htwg.sudoku.controller.ISudokuController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainController extends Controller {
 	static ISudokuController controller = Sudoku.getInstance().getController();
@@ -30,29 +37,29 @@ public class MainController extends Controller {
         Sudoku.getInstance().getTUI().processInputLine(command);
         return ok(controller.getGrid().toJson());
     }
-    
+
     public static WebSocket<String> connectWebSocket() {
         return new WebSocket<String>() {
-       
+
             public void onReady(WebSocket.In<String> in, WebSocket.Out<String> out) {
             	new GridObserver(controller,out);
             }
 
         };
     }
-    
+
     public static Result login() {
         return ok( views.html.login.render(Form.form(User.class)));
     }
-    
+
     public static Result logout() {
     	session().clear();
     	return redirect(routes.MainController.index());
     }
-    
+
     public static Result authenticate() {
         Form<User> loginform = DynamicForm.form(User.class).bindFromRequest();
-        
+
         User user = User.authenticate(loginform.get());
 
         if (loginform.hasErrors() || user == null) {
@@ -64,19 +71,19 @@ public class MainController extends Controller {
             return redirect(
                 routes.MainController.index()
             );
-        } 
+        }
     }
-    
+
     public static class User {
-        
+
         private final static String defaultUser = "test@123.de";
         private final static String defaultPasswort = "nsa";
 
         public String email;
         public String password;
-        
+
         public User() {}
-        
+
         private User(final String email, final String password) {
             this.email = email;
             this.password = password;
@@ -86,11 +93,11 @@ public class MainController extends Controller {
      	    if (user.email.equals(defaultUser) && user.password.equals(defaultPasswort)) {
      	        return new User(user.email, user.password);
      	    }
-     	    
+
     	    return null;
     	}
    }
-   
+
     public static class Secured extends Security.Authenticator {
 
         @Override
@@ -104,4 +111,26 @@ public class MainController extends Controller {
         }
     }
 
+    public static Result auth() {
+        String providerUrl = "https://www.google.com/accounts/o8/id";
+        String returnToUrl = "http://localhost:9000/openID/verify";
+
+        Map<String, String> attributes = new HashMap<>();
+        attributes.put("Email", "http://schema.openid.net/contact/email");
+        attributes.put("FirstName", "http://schema.openid.net/namePerson/first");
+        attributes.put("LastName", "http://schema.openid.net/namePerson/last");
+
+        F.Promise<String> redirectUrl = OpenID.redirectURL(providerUrl, returnToUrl, attributes);
+        return redirect(redirectUrl.get());
+    }
+
+    public Result verify() {
+        F.Promise<OpenID.UserInfo> userInfoPromise = OpenID.verifiedId();
+        OpenID.UserInfo userInfo = userInfoPromise.get();
+        session().clear();
+        session("email", userInfo.attributes.get("Email"));
+        return redirect(
+                routes.MainController.index()
+        );
+    }
 }
